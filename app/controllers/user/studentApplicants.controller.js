6b67a493ab5fd8f6ac69d9490f74a19b68cb8aa4
Muhammad Shahzeb <mshahzeb793@gmail.com>
2021-10-06 10:39:33 +0500

    set dignature image upload

--- app/controllers/user/studentApplicants.controller.js
+++ app/controllers/user/studentApplicants.controller.js
@@ -1,6 +1,7 @@
 const constants = require('../../helpers/constants.helpers');
 const studentApplicationService = require("../../services/studentApplication.service");
 const apiResp = require('../../helpers/apiResponse.helper');
+const expressFile = require('../helpers/expressFileUpload.helper')
 
 
 module.exports = {
@@ -10,15 +11,39 @@ module.exports = {
       declaration, educationDetails, educationQualification, email,
       englishProficiency, gender, guardian, homeAddress, name,
       nationality, statement } = req.body
+    let studentSign = '';
+    if (req.files) {
+      const result = await expressFile.uploadFile(req.files.signature, process.env.studentSignaturePath)
+      if (!result.status) {
+        throw new Error(result.message)
+      }
+      studentSign = result.message
+    }
+    const application = {
+      applicationDetails,
+      contact,
+      countryOfBirth,
+      dateOfBirth,
+      declaration: {
+        date: declaration.date,
+        firstName: declaration.firstName,
+        lastName: declaration.lastName,
+        middleName: declaration.middleName,
+        signature: studentSign
+      },
+      educationDetails,
+      educationQualification,
+      email,
+      englishProficiency,
+      gender,
+      guardian,
+      homeAddress,
+      name,
+      nationality,
+      statement
+    }
 
-    // if (req.files) {
-    //   const result = await expressFile.uploadFile(req.files.signature, process.env.studentSignaturePath)
-    //   if (!result.status) {
-    //     throw new Error(result.message)
-    //   }
-    //   const signature = result.message
-    // }
-    const data = await studentApplicationService.create(req.body)
+    const data = await studentApplicationService.create(application)
 
     apiResp.sendData(res, data, constants.DATA_LOADED)
   }
