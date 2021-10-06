836c90f93073bf7cf020f1b5eaa5b1939dd642c2
Muhammad Shahzeb <mshahzeb793@gmail.com>
2021-10-06 10:30:13 +0500

    working on student form

--- app/controllers/user/studentApplicants.controller.js
+++ app/controllers/user/studentApplicants.controller.js
@@ -11,6 +11,13 @@ module.exports = {
       englishProficiency, gender, guardian, homeAddress, name,
       nationality, statement } = req.body
 
+    // if (req.files) {
+    //   const result = await expressFile.uploadFile(req.files.signature, process.env.studentSignaturePath)
+    //   if (!result.status) {
+    //     throw new Error(result.message)
+    //   }
+    //   const signature = result.message
+    // }
     const data = await studentApplicationService.create(req.body)
 
     apiResp.sendData(res, data, constants.DATA_LOADED)
